40c259fa45482d28d5ba9d5b2c4f30e67aac1d41
Rishabh-rb <rishabh.barnwal_cs.aiml24@gla.ac.in>
2026-02-20 09:36:01 +0530

    Add RGB LED control with digital and PWM modes for Uno R4 WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,42 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author rishabh-rb
+ * @date 2026-02-20
  *
  * @details
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
+// TODO 1:
+// Define RED pin (Use 9)
+#define RED_PIN 9
 
- // TODO 2:
- // Define GREEN pin (Use 10)
+// TODO 2:
+// Define GREEN pin (Use 10)
+#define GREEN_PIN 10
 
- // TODO 3:
- // Define BLUE pin (Use 11)
+// TODO 3:
+// Define BLUE pin (Use 11)
+#define BLUE_PIN 11
 
 void setup() {
 
     // TODO 4:
     // Initialize Serial communication (9600 baud)
+    Serial.begin(9600);
 
     // TODO 5:
     // Configure RGB pins as OUTPUT
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
     // TODO 6:
     // Print initialization message
+    Serial.println("RGB LED Control System - Initialized");
+    delay(1000);
 }
 
 void loop() {
@@ -37,21 +46,37 @@ void loop() {
 
     // TODO 7:
     // Turn ON red (digital HIGH)
+    digitalWrite(RED_PIN, HIGH);
+    delay(1000);
 
     // TODO 8:
     // Turn OFF red
+    digitalWrite(RED_PIN, LOW);
+    delay(1000);
 
     // -------- ANALOG (PWM) MODE --------
 
     // TODO 9:
     // Set RED brightness using analogWrite()
+    analogWrite(RED_PIN, 255);   // Full brightness
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 0);
+    delay(1000);
 
     // TODO 10:
     // Set GREEN brightness using analogWrite()
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 255);
+    analogWrite(BLUE_PIN, 0);
+    delay(1000);
 
     // TODO 11:
     // Set BLUE brightness using analogWrite()
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 255);
 
     // TODO 12:
     // Add delay for visible transition
+    delay(1000);
 }
